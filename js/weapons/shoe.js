63af80327bc6c9f44adeebef066f9566463aa2aa
Gonzalo Delgado <mail@gonzalodelgado.com>
2019-02-26 20:04:43 -0300

    Create shoe weapon class.

---
+++ js/weapons/shoe.js
@@ -0,0 +1,8 @@
+shoeClass.prototype = new rangedWeaponClass()
+function shoeClass() {
+	this.name = "shoe";
+	this.pluralName = "shoes";
+	this.baseDamage = 0.3;
+	this.quantity = 2;
+	this.color = "black";
+}
