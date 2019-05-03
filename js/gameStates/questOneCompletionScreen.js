c495589dd1ac6a9f24ba03940b90c7271e25788d
vmckeown <vmckeown79@gmail.com>
2019-05-03 07:05:56 -0400

    Quest 1 Completion Screen.  Fixed Text going over the edge of scroll.

--- js/gameStates/questOneCompletionScreen.js
+++ js/gameStates/questOneCompletionScreen.js
@@ -23,9 +23,10 @@ function drawQuestOneCompletionScreen() {
   canvasContext.translate(stateScreenOffsetX, stateScreenOffsetY);
   canvasContext.drawImage(scrollBackgroundPic, 0, 0);
   colorText("Falldale Saved", 310, 75, "blue", font = "bold 42px Georgia");
-  colorText("You have saved your lovely town of Falldale of the beasts roaming the streets.", 115, 150, "green", font = "20px Georgia");
-  colorText("You wonder what has happened in your absence?", 150, 200, "green", font = "20px Georgia");
-  colorText("Comforted of your town being safe, you want to go check on your Princess.", 153, 250, "green", font = "20px Georgia");
+  colorText("You have saved your lovely town of Falldale of the beasts roaming", 115, 150, "green", font = "20px Georgia");
+  colorText(" the streets.  You wonder what has happened in your absence?", 150, 200, "green", font = "20px Georgia");
+  colorText("Comforted of your town being safe, you want to go check on", 153, 250, "green", font = "20px Georgia");
+   colorText("your Princess.", 153, 250, "green", font = "20px Georgia");
   //Feel free to edit/update/change to this storyline.
   canvasContext.restore();
 }
